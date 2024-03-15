f5b9a468d3ce169b6ce73deb2e8597e5b052c9c6
Fred <clyckov80@gmail.com>
2024-03-15 15:19:36 +0300

    commite

--- parser.py
+++ parser.py
@@ -9,16 +9,20 @@ soup = BeautifulSoup(request.text,"html.parser")
 
 with open("links.txt", "w+",encoding="UTF-8") as file3:
     links = soup.find_all("a")
-    print(f"ССЫЛКИ {'-'*100} \n{links}")
+    for i in links:
+        print(i)
     file3.write(f"{links}")
 print("_" * 100)
 with open("image.txt", "w+",encoding="UTF-8") as file2:
     image = soup.find_all("img")
-    print(f"КАРТИНКИ {'_'*85} \n{image}")
+    for i in image:
+        print(i)
+    print("_" * 100)
     file2.write(f"{image}")
 with open("class.txt", "w+",encoding="UTF-8") as file1:
     clas = soup.find_all(class_=True)
-    #print(f"КЛАССЫ {'_' * 85} \n{clas}")
+
+    a = [print(i) for i in clas]
     file1.write(f"{clas}")
 
 with open("text.txt", "w+",encoding="UTF-8") as file:
